0493c8302e38c86c0ca414d23e55739c4cfedc08
migsan1104 <m1104s2003@gmail.com>
2025-07-28 21:54:18 -0400

    Comment changes

--- src/Forwarding_Unit.sv
+++ src/Forwarding_Unit.sv
@@ -10,20 +10,20 @@ module Forwarding_Unit(
 );
 
 always_comb begin
-    // by default we assume no fowarding is needed
+    // by default we assume no forwarding is needed
     ForwardA_Ex = 2'b00;
     ForwardB_Ex = 2'b00;
     ForwardA_ID = 2'b00;
     ForwardB_ID = 2'b00;
 
     // this first set of if statements only apply to fowarding to the exe stage
-    // fowarding from mem to exe
+    // forwarding from mem to exe
     if (EX_MEM_RegWrite && (EX_MEM_rd != 0) && (EX_MEM_rd == ID_EX_rs))
         ForwardA_Ex = 2'b01;
     if (EX_MEM_RegWrite && (EX_MEM_rd != 0) && (EX_MEM_rd == ID_EX_rt))
         ForwardB_Ex = 2'b01;
 
-    // This section is to foward from wb to exe
+    // This section is to forward from wb to exe
     if (MEM_WB_RegWrite && (MEM_WB_rd != 0) &&
         !(EX_MEM_RegWrite && (EX_MEM_rd != 0) && (EX_MEM_rd == ID_EX_rs)) &&
         (MEM_WB_rd == ID_EX_rs))
@@ -34,7 +34,7 @@ always_comb begin
         (MEM_WB_rd == ID_EX_rt))
         ForwardB_Ex = 2'b10;
 
-    // this set of if statement is for fowarding to the ID stage, this is used in branch resolution
+    // this set of if statement is for forwarding to the ID stage, this is used in branch resolution
     // for inputA
     if (ID_EX_RegWrite && (ID_EX_rd != 0) && (ID_EX_rd == IF_ID_rs))
         ForwardA_ID = 2'b01;
